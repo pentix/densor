c226f33c84394cb6422c3ba5e51ae79b35d78758
Patrick Eigensatz <patrick.eigensatz@gmail.com>
2020-02-15 20:23:52 +0100

    Show remote sensors in dashboard

---
+++ dashboard.go
@@ -0,0 +1,75 @@
+package main
+
+import "fmt"
+
+func showDashboard() {
+	fmt.Println("\033[2J\033[H")
+	fmt.Println("------------------------------------------------------------------------------------------------------------")
+	fmt.Println("\033[1mInstance                                 Sensor                          Status     Last Update\033[0m")
+	fmt.Println("------------------------------------------------------------------------------------------------------------")
+
+	// Local sensors first
+	for _, sensor := range local.sensors {
+		printSensorStatus(local.DisplayName, sensor)
+	}
+
+	// Then ordered by remote instances
+	for _, remote := range local.RemoteInstances {
+		for _, sensor := range remote.sensors {
+			printSensorStatus(remote.DisplayName, sensor)
+		}
+	}
+
+	fmt.Println("------------------------------------------------------------------------------------------------------------")
+
+	// Remote instances
+	fmt.Println("\n\n")
+	fmt.Println("------------------------------------------------------------------------------------------------------------")
+	fmt.Println("\033[1mRemote Instance                          Status       \033[0m")
+	fmt.Println("------------------------------------------------------------------------------------------------------------")
+
+	for _, remote := range local.RemoteInstances {
+		printRemoteInstanceStatus(&remote)
+	}
+
+	fmt.Println("------------------------------------------------------------------------------------------------------------")
+
+}
+
+func printSensorStatus(instanceDisplayName string, sensor *Sensor) {
+	var statusText string
+	var colorCode string
+
+	if sensor.lastUpdateStatus() {
+		statusText = "OK"
+		colorCode = "\033[32m"
+	} else {
+		statusText = "FAIL"
+		colorCode = "\033[31m"
+	}
+
+	fmt.Printf("%s%-35s      %-22s          %-4s       %s\033[0m\n",
+		colorCode,
+		instanceDisplayName,
+		sensor.DisplayName,
+		statusText,
+		sensor.lastUpdateTimestamp())
+}
+
+func printRemoteInstanceStatus(remote *RemoteInstance) {
+	var statusText string
+	var colorCode string
+
+	if remote.connected {
+		statusText = "Connected"
+		colorCode = "\033[32m"
+	} else {
+		statusText = "Not Connected"
+		colorCode = "\033[31m"
+	}
+
+	fmt.Printf("%s%-35s      %-22s\033[0m\n",
+		colorCode,
+		remote.DisplayName,
+		statusText)
+}
--- densor.go
+++ densor.go
@@ -58,64 +58,6 @@ func main() {
 	// select {}
 }
 
-func showDashboard() {
-	fmt.Println("\033[2J\033[H")
-	fmt.Println("------------------------------------------------------------------------------------------------------------")
-	fmt.Println("\033[1mInstance                                 Sensor                          Status     Last Update\033[0m")
-	fmt.Println("------------------------------------------------------------------------------------------------------------")
-
-	// Local sensors first
-	for _, sensor := range local.sensors {
-		var statusText string
-		var colorCode string
-
-		if sensor.lastUpdateStatus() {
-			statusText = "OK"
-			colorCode = "\033[32m"
-		} else {
-			statusText = "FAIL"
-			colorCode = "\033[31m"
-		}
-
-		fmt.Printf("%s%-35s      %-22s          %-4s       %s\033[0m\n",
-			colorCode,
-			local.DisplayName,
-			sensor.DisplayName,
-			statusText,
-			sensor.lastUpdateTimestamp())
-	}
-	fmt.Println("------------------------------------------------------------------------------------------------------------")
-
-	// Remote instances
-
-	fmt.Println("\n\n")
-	fmt.Println("------------------------------------------------------------------------------------------------------------")
-	fmt.Println("\033[1mRemote Instance                          Status       \033[0m")
-	fmt.Println("------------------------------------------------------------------------------------------------------------")
-
-	for _, remote := range local.RemoteInstances {
-
-		var statusText string
-		var colorCode string
-
-		if remote.connected {
-			statusText = "Connected"
-			colorCode = "\033[32m"
-		} else {
-			statusText = "Not Connected"
-			colorCode = "\033[31m"
-		}
-
-		fmt.Printf("%s%-35s      %-22s\033[0m\n",
-			colorCode,
-			remote.DisplayName,
-			statusText)
-	}
-
-	fmt.Println("------------------------------------------------------------------------------------------------------------")
-
-}
-
 func debug() {
 	for {
 		//	logger.Println(local.RemoteInstances, "\n\n\n")
